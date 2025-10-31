2bca7959e42dc8febb96d08105a445807553a20d
kirtidhwaj <kirtidhwajpatra@gmail.com>
2025-10-31 10:51:24 +0530

    resolved

--- 700/A_Game.cpp
+++ 700/A_Game.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 
 int main(){
+    int n;
+    cin>>n;
 
+    vector<int> a(n);
+    for(int i = 0; i<n; i++){
+        cin>>a[i];
+    }
+    sort(a.begin(), a.end());
+    bool firstPlayer = true;
 
+    while(a.size()>1){
+        if(firstPlayer==true){
+            a.pop_back();
+        } else{
+            a.erase(a.begin());
+        }
+        if(firstPlayer==true)firstPlayer=false;
+        else firstPlayer=true;
+        
+    }
+    cout<<a[0]<<endl;
 
     return 0;
 }
